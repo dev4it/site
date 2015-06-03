fc8451925f5c3a77bad410edb4f62ab8554224dd
GuillaumeFE <guillaume.fesquet@dev4it.fr>
2015-06-03 12:14:40 +0200

    section dev4you
    remise en forme
    correction bugs IE et positionnement scroll
    Attention sous IE les fab n’ont pas de pointer…
    gestion de l’ensemble du javascript dorénavant dans Angular (toggle
    menu, +scroll)

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,9 +5,31 @@
 
   var dev4itCtrls = angular.module('dev4itCtrls', []);
 
-  dev4itCtrls.controller('mainCtrl', ['$scope', '$location',
-    function($scope, $location) {
+  dev4itCtrls.controller('mainCtrl', ['$scope', '$location', '$anchorScroll',
+    function($scope, $location, $anchorScroll) {
       $scope.$location = $location;
+
+      $scope.gotoTarget = function(target) {
+
+        $anchorScroll.yOffset = angular.element(document.querySelector('header'))[0].offsetHeight - 24; // 48 the dimension of the md-fab arrow
+
+        $location.hash(target);
+
+        $anchorScroll();
+      };
+
+      $scope.toggled = false;
+      $scope.isToggled = function() {
+        return $scope.toggled;
+      };
+
+      $scope.toggle = function() {
+        $scope.toggled = !$scope.toggled;
+      };
+
+      $scope.isSelected = function(href) {
+        return $location.path() === href;
+      };
     }
   ]);
 
